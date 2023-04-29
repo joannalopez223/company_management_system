c9b81b8e0bd4f0a206a7ec65926ebcd74a4f66bb
Joanna Lopez <joannalopez@nevada.unr.edu>
2023-04-29 16:23:36 -0700

    adding company_management_systems

---
+++ company_mangement_system/lib/company_mangement_system/store_management_system/product.ex
@@ -0,0 +1,22 @@
+defmodule CompanyMangementSystem.StoreManagementSystem.Product do
+  use Ecto.Schema
+  import Ecto.Changeset
+
+  alias CompanyMangementSystem.StoreManagementSystem.Store
+  schema "products" do
+    field :inventoryCount, :integer
+    field :retailPrice, :float
+    field :vendor, :string
+    field :wholesalePrice, :float
+    field :name, :string
+    belongs_to :store, Store
+    timestamps()
+  end
+
+  @doc false
+  def changeset(product, attrs) do
+    product
+    |> cast(attrs, [:vendor, :wholesalePrice, :retailPrice, :inventoryCount, :store_id, :name])
+    |> validate_required([:vendor, :wholesalePrice, :retailPrice, :inventoryCount, :store_id, :name])
+  end
+end
---
+++ company_mangement_system/priv/repo/migrations/20230422215219_create_stores.exs
@@ -0,0 +1,13 @@
+defmodule CompanyMangementSystem.Repo.Migrations.CreateStores do
+  use Ecto.Migration
+
+  def change do
+    create table(:stores) do
+      add :location, :string
+      add :store_id, :integer
+      
+
+      timestamps()
+    end
+  end
+end
